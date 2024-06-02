ebb6cc9f38b3746d2fbd3cc42ed53ffe5f40111c
harimasa <144851980+NecroPig@users.noreply.github.com>
2024-06-02 20:03:21 +0300

    Add files via upload

---
+++ src/main/java/ru/femboypig/modules/combat/HitArmor.java
@@ -0,0 +1,9 @@
+package ru.femboypig.modules.combat;
+
+import ru.femboypig.modules.Func;
+
+public class HitArmor extends Func {
+    public HitArmor() {
+        super("Hit Armor", "Allows you to change the color of your armor when hit.");
+    }
+}
