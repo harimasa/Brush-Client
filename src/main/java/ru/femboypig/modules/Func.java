6d0bcb9d055bfe3aade72d40b1f485a0d844d093
harimasa <144851980+NecroPig@users.noreply.github.com>
2024-06-02 05:58:08 +0300

    Delete src/main directory

--- src/main/java/ru/femboypig/modules/Func.java
+++
@@ -1,7 +0,0 @@
-package ru.femboypig.modules;
-
-public class Func extends Module {
-    public Func(String name, String description) {
-        super(name, description);
-    }
-}
--- src/main/java/ru/femboypig/utils/listeners/listener.java
+++
@@ -1,7 +0,0 @@
-package ru.femboypig.utils.listeners;
-
-import ru.femboypig.utils.interfaces.instance;
-
-public interface listener extends instance {
-
-}
