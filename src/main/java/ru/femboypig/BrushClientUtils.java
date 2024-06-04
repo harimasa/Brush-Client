c00449e25bbb78b21c6cf816407819dbdc8dda9f
harimasa <144851980+NecroPig@users.noreply.github.com>
2024-06-04 19:56:01 +0300

    new modules

---
+++ src/main/java/ru/femboypig/BrushClientUtils.java
@@ -0,0 +1,12 @@
+package ru.femboypig;
+
+public class BrushClientUtils {
+    public static String getAuthors() {
+        String contributors = """
+                \n\t Contributors:
+                \t    Developer: femboyPig,
+                \t    Assets Manager: Drimbik
+                """;
+        return contributors;
+    }
+}
---
+++ src/main/java/ru/femboypig/mixins/menu/MixinDrawContext.java
@@ -0,0 +1,25 @@
+package ru.femboypig.mixins.menu;
+
+import net.minecraft.client.font.TextRenderer;
+import net.minecraft.client.gui.DrawContext;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import ru.femboypig.config.BrushCC;
+
+@Mixin(DrawContext.class)
+public class MixinDrawContext {
+    @Shadow
+    public int drawText(TextRenderer textRenderer, @Nullable String string, int i, int j, int k, boolean bl) {
+        return 0;
+    }
+
+    @Inject(at = @At("HEAD"), method = "drawTextWithShadow(Lnet/minecraft/client/font/TextRenderer;Ljava/lang/String;III)I", cancellable = true)
+    public void drawTextWithShadow(TextRenderer textRenderer, String string, int i, int j, int k, CallbackInfoReturnable<Integer> cir) {
+        if (string.startsWith("Minecraft 1.20.1/Fabric ("))
+            cir.setReturnValue(drawText(textRenderer, "", i, j, k, true));
+    }
+}
